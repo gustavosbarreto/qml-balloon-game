f4014a6aeb9ca5d9d4681c4b230cd1610616a8a7
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2013-08-27 13:53:38 -0300

    Game logic engine

---
+++ qml/qml-balloon-game/GameLogic.js
@@ -0,0 +1,10 @@
+var balloons = new Array();
+
+function collisionDetect() {
+    for (var i = 0; i < balloons.length; i++) {
+        var dart = game.getDartPosition();
+        if (dart.x + 96 > balloons[i].x && dart.x < balloons[i].x)
+            if (dart.y > balloons[i].y && dart.y < balloons[i].y + 120)
+                balloons[i].explode();
+    }
+}
